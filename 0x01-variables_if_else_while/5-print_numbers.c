c987945875e00eb8990f9579a08ae3a569786e23
mogi <nyabokenaomi001@gmail.com>
2023-02-17 17:18:14 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the ain function, kwani huskii
+ *
+ * Return: is a zero ama sufuri
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
