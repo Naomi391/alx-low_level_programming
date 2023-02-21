bcb0a088bb2840cfdb084d74b6b1d46309290022
mogi <nyabokenaomi001@gmail.com>
2023-02-21 07:34:56 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - is the main function
+ * _putchar: is the one that prints alphabet putchar
+ * Return: is a zero or sufuri
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
