496c16d990e32b80ba497bf0a24602a305817837
mogi <nyabokenaomi001@gmail.com>
2023-02-21 18:16:18 -0800

    Adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - the function adds the integers
+ *@m: is parameter one
+ *@n: is parameter two
+ *
+ * Return: m + n value
+ */
+
+int add(int m, int n)
+{
+	return (m + n);
+}
