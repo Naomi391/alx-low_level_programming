f6053c6f4586202d8ccc78519de7a3a338ddd65b
mogi <nyabokenaomi001@gmail.com>
2023-02-22 08:13:54 -0800

    Task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * 
+ * Return: is a zero or 0
+ */
+
+int main(void)
+{
+	int a;
+	int sum;
+
+	sum = 0;
+
+	for (a = 0 ; a < 1024 ; a++)
+	{
+		if (( a % 3 == 0 || a % 5 == 0));
+		{
+			sum = sum + a;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
