5de0a1ce7ec197957ed6f4727ec3e8d85c64de88
mogi <nyabokenaomi001@gmail.com>
2023-02-22 07:38:07 -0800

    Task 5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
- *_isalpha - is a function that prints both upper and lower case alphabets
+ * _isalpha - checks for alphabets
+ * @c: parameters to be checked
  *
- *@c: is the paraet eter to be printed
- *
- * Return: zero or 1 if it is an alphabet and 0 if otherwise
+ * Return: 1 if it is an alphabet
+ * and 0 if otherwise
  */
+
 
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122)) || (c >= 65 && c <= 90))
+		if ((c >= 97 && c <= 122))
+		'||' ((c >= 65 && c <= 90))
 		return (1);
 	else
 		return (0);
