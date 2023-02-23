f46b840a53429d3d47dc5288ddd2af11dd9cdab4
mogi <nyabokenaomi001@gmail.com>
2023-02-23 06:35:50 -0800

    Function that multuplies

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - this multiplies two integers
+ * @a: is the first integer
+ * @b: is the second integer
+ *
+ * Return: product of @a * @b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
