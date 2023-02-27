6039c4880bf0061143f2b018d7d2bdb52f961904
mogi <nyabokenaomi001@gmail.com>
2023-02-27 14:31:31 -0800

    Task nine

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - prints elements of an array of integers
+ * @a: array name
+ * @n: number of elements
+ *
+ * Return: a and n inputs
+ */
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < (n - 1); x++)
+	{
+		printf("%d,", a[x]);
+	}
+		if (x == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+		printf('\n');
+}
