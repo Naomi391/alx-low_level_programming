e2c963fcb5f1b39e85f595038d429936b0d2ddb2
mogi <nyabokenaomi001@gmail.com>
2023-03-07 06:13:58 -0800

    Tassk

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * _memecpy - function that copies memory
+ * _memset - function that copies memory
  * @s: startong address
  * @b: desired value
  * @n: no of bytes to be changed
- *
- * Return: changed array 
+ * Return: changed array
  */
- 
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
