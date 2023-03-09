4ef1576973933131a9ea3c72f59500a88e685a7d
mogi <nyabokenaomi001@gmail.com>
2023-03-09 01:59:47 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,17 @@
 #include "main.h"
-
 /**
- * _puts_recursion - puts like function
- * @s: input value
- *
- * Return: always zero on success
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar (*s);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+
 	else
-		_putchar ('\n');
+		_putchar('\n');
 }
