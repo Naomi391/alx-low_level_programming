effae7def442a3048234d51883f5f3406048e8f6
mogi <nyabokenaomi001@gmail.com>
2023-03-09 03:54:38 -0800

    Task five

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+int actual_sqrt_recursion(int n, int i);
+
+/**
+ * _sqrt_recursion - returns square
+ * @n: number to calculate square
+ *
+ * Return: square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recurces to find natural
+ * square root of a number
+ * @n: number to calc the square root
+ * @i: iterator
+ *
+ * Return: resulting square root
+ */
+
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}
