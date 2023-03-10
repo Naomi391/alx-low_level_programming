86ff34b9b15505ca7d17877ea1fb01bd53955f95
mogi <nyabokenaomi001@gmail.com>
2023-03-10 06:19:40 -0800

    Stat

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,15 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
 
+/**
+ * _puts - prints a string to stdout
+ * @str: parameter to be printed
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,14 +1,25 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
 /**
- * main - Entry point
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
  *
- * Return: Always 0 (Success)
+ * Return: s1[i] - s2[i]
  */
-int main(void)
+int _strcmp(char *s1, char *s2)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	int i;
+
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	i++;
+	}
 	return (0);
 }
 
