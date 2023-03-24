92b29613878d55a5bae6a80801871ee1a9121724
mogi <nyabokenaomi001@gmail.com>
2023-03-23 19:35:15 -0700

    Function that prints strings using stdarg.h

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list strings;
 	char *str;
 	unsigned int index;
-	
+
 	va_start(strings, n);
 	for (index = 0; index < n; index++)
 	{
@@ -25,11 +25,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", str);
-		
+
 		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
+
 	va_end(strings);
 }
 
